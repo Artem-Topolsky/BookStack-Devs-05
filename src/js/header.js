8d5da068e7c4318632539e4e96baf2d6e214906c
IuriiGred <gredasoviurii@gmail.com>
2025-07-04 20:05:34 +0200

    first commit

---
+++ src/js/header.js
@@ -0,0 +1,35 @@
+const burgerBtn = document.querySelector(".burger-menu");
+const openNavbar = document.querySelector(".navbar-overlay");
+const closeNavbarBtn = document.querySelector(".close-navbar-btn");
+const choiceSectionInHeader = document.querySelector("ul.header-list")
+const choiceSectionInNavbar = document.querySelector("ul.navbar-list");
+
+burgerBtn.addEventListener("click", clickOpenNavbar);
+closeNavbarBtn.addEventListener("click", closeNavbar);
+
+choiceSectionInNavbar.addEventListener("click", handlerNavbarChoice);
+choiceSectionInHeader.addEventListener("click", handlerHeaderChoice);
+
+function clickOpenNavbar() {
+    openNavbar.classList.remove("is-hidden");
+    console.log(window.innerHeight, window.innerWidth);
+};
+
+function handlerNavbarChoice(event) {
+    console.dir(event.target.dataset.id);
+    closeNavbar();
+};
+
+function handlerHeaderChoice(event) {
+    console.dir(event.target.dataset.id);
+};
+
+function closeNavbar() {
+    openNavbar.classList.add("is-hidden");
+}
+
+console.log(window.innerHeight, window.innerWidth);
+
+console.log(document.documentElement.scrollHeight);
+
+console.log(document.documentElement.clientHeight);
